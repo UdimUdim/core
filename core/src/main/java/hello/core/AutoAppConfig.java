e2a1eadc1ad729b8cfc9691dc3c58f9754c9306c
Yurim Oh <ohyr970913@gmail.com>
2022-01-23 17:59:00 +0900

    다양한 의존관계 주입 방법

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,13 +1,25 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
-        excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //@Configuration이 붙은 class는 제외한다(이 프로젝트에선 AppConfig, TestConfig 해당)
+        basePackages = "hello.core",
+        excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //@Configuration이 붙은 class는 제외한다(이 프로젝트에선 AppConfig, TestConfig 해당)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 
 }
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,6 +2,7 @@ package hello.core.scan;
 
 import hello.core.*;
 import hello.core.member.*;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.context.annotation.*;
@@ -14,5 +15,9 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
