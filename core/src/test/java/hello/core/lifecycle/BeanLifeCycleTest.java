529a69511bb7d4cfa264e8762696de18fe592198
olivia <ohyr970913@gmail.com>
2022-02-04 17:55:49 +0900

    빈 생명주기 콜백

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,25 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.*;
+import org.springframework.context.*;
+import org.springframework.context.annotation.*;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
